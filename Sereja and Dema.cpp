a06fdfda79f32f8b19f94a4b2bbe86c33e5f2e20
knfm2312 <yabsr23@gmail.com>
2025-02-08 18:59:16 +0300

    Create Sereja and Dema.cpp

---
+++ Sereja and Dema.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int cards[1000];
+	int sereja = 0;
+	int dima = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> cards[i];
+	}
+	int pre = 0; 
+	int post = n - 1;
+	bool serejapick = true;
+	for (int i = 0; i < n; i++) {
+		if (cards[pre] > cards[post]) {
+			if (serejapick) {
+				sereja += cards[pre];
+			}
+			else {
+				dima += cards[pre];
+			}
+			pre++;
+		}
+		else {
+			if (serejapick) {
+				sereja += cards[post];
+			}
+			else {
+				dima += cards[post];
+			}
+			post--;
+		}
+		serejapick = !serejapick;
+	}
+	
+	cout << sereja << " " << dima;
+}
