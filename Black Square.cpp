9bce5420dd7a793134193b6958724ce7b8813363
knfm2312 <yabsr23@gmail.com>
2025-02-08 23:47:29 +0300

    Create Black Square.cpp

---
+++ Black Square.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+	int arry[10000];
+	int aa;
+	int count = 0;
+	int look=0;
+	int callory = 0;
+	string strip;
+	while (cin >> aa) {
+		arry[count] = aa;
+		count++;
+		if (cin.peek() == '\n') {
+			break;
+		}
+	}
+	
+	cin >> strip;
+	for (char& c : strip) {
+		
+		int num = c - '0';
+		num--;
+		look += arry[num];
+	}
+	cout<<look;
+}
