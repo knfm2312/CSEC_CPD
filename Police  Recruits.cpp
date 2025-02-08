6eec91ed9c32717d13c9e8d793834bb99e1461f5
knfm2312 <yabsr23@gmail.com>
2025-02-08 19:58:23 +0300

    Create Police  Recruits.cpp

---
+++ Police  Recruits.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int event[100000];
+	int crime = 0;
+	int inv = 0;
+	for (int i = 0;i < n;i++) {
+		cin >> event[i];
+		if (event[i] < 0) {
+			if (inv > 0) {
+				inv = inv + event[i];
+			}
+			else {
+				crime++;
+			}
+		}
+		else{
+			inv=inv+ event[i];
+		}
+
+	}
+	cout << crime;
+}
